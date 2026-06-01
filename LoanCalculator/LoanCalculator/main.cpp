02a5da37d6e552425df543aac866a8e3d3dfbe02
AlejandroT23 <atoledo1@carthage.edu>
2026-06-01 17:18:09 -0500

    Initial Setup for files

---
+++ LoanCalculator/LoanCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LoanCalculator
+//
+//  Created by Alejandro Toledo on 6/1/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
